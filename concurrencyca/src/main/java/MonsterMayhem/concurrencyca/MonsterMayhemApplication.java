d0a9f79d235018c0c71f02223dc0af7732b7e8e7
Enio Kelsio <152182706+EnioKelsioV@users.noreply.github.com>
2024-05-27 21:53:45 +0100

    firstComit

---
+++ concurrencyca/src/main/java/MonsterMayhem/concurrencyca/MonsterMayhemApplication.java
@@ -0,0 +1,13 @@
+package MonsterMayhem.concurrencyca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterMayhemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterMayhemApplication.class, args);
+	}
+
+}
---
+++ concurrencyca/src/test/java/MonsterMayhem/concurrencyca/ConcurrencycaApplicationTests.java
@@ -0,0 +1,13 @@
+package MonsterMayhem.concurrencyca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencycaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
